bd3644cb4cb94c64b9d98473da1532347cbb6f47
Sunny <swarnendughosh05@gmail.com>
2024-07-31 20:55:37 +0530

    animation added in side bar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -11,22 +11,31 @@ const pathname = usePathname();
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4'>
             <Link href='/' className='sidebar-logo'>
+            <span className='ml-2.5'>
                 <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28}/>
+            </span>
             </Link>
             <nav className='sidebar-nav'>
                 <SignedIn>
-                    <ul className='sidebar-nav_elements'>
+                    <ul className='sidebar-nav_elements '>
                             {navLinks.map((link) => {
                                 const isActive = link.route === pathname
                                 return (
-                                    <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                                    <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700 transition ease-in-out delay-10 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-slate-200 duration-10 ...'}`}>
+                                        <Link className='sidebar-link' href={link.route}>
+                                        <Image src={link.icon} alt='logo'
+                                        width={24}
+                                        height={24}
+                                        className={`${isActive && 'brightness-200'}`}
+                                        />
                                         {link.label}
+                                        
+                                        </Link>
                                     </li>
                                 )
                             })}
                     </ul>
                 </SignedIn>
-
             </nav>
         </div>
     </aside>
