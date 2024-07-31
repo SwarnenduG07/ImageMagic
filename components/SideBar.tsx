eb617a3901d77ff3e55816b3c7f3f738bb455df5
Sunny <swarnendughosh05@gmail.com>
2024-07-31 21:37:24 +0530

    COnstant sliced

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,10 @@
 "use client"
 import  Link  from 'next/link'
 import Image from "next/image"
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import { Button } from './ui/button'
 
 export const SideBar = () => {
 const pathname = usePathname();
@@ -17,8 +18,8 @@ const pathname = usePathname();
             </Link>
             <nav className='sidebar-nav'>
                 <SignedIn>
-                    <ul className='sidebar-nav_elements '>
-                            {navLinks.map((link) => {
+                    <ul className='sidebar-nav_elements'>
+                            {navLinks.slice(0,6).map((link) => {
                                 const isActive = link.route === pathname
                                 return (
                                     <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700 transition ease-in-out delay-10 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-slate-200 duration-10 ...'}`}>
@@ -34,8 +35,35 @@ const pathname = usePathname();
                                     </li>
                                 )
                             })}
-                    </ul>
+                            </ul>
+                         <ul className='sidebar-nav_elements'>   
+                         {navLinks.slice(6).map((link) => {
+                                const isActive = link.route === pathname
+                                return (
+                                    <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700 transition ease-in-out delay-10 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-slate-200 duration-10 ...'}`}>
+                                        <Link className='sidebar-link' href={link.route}>
+                                        <Image src={link.icon} alt='logo'
+                                        width={24}
+                                        height={24}
+                                        className={`${isActive && 'brightness-200'}`}
+                                        />
+                                        {link.label}
+                                        
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                            <li className='flex-center cursor-pointer gap-2 p-4 transition ease-in-out delay-10 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-slate-200 duration-10 rounded-full'>
+                                    <UserButton afterSwitchSessionUrl='/' showName />
+                            </li>
+                        </ul>
                 </SignedIn>
+            
+                <SignedOut>
+                    <Button asChild className='button bg-purple-gradient bg-cover transition ease-in-out delay-150 bg-emerald-500 hover:-translate-y-1 hover:scale-100 hover:bg-indigo-500 duration-200 ...'>
+                        <Link href="/sign-in">Login</Link>
+                    </Button>
+                </SignedOut>
             </nav>
         </div>
     </aside>
