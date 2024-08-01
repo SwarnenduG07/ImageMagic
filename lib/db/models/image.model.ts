525bf6f102f337cb5198fe2d55e4def1815f1bdc
Sunny <swarnendughosh05@gmail.com>
2024-08-01 21:42:44 +0530

    monggose interface added

--- lib/db/models/image.model.ts
+++ lib/db/models/image.model.ts
@@ -1,6 +1,27 @@
 
 import { model, models, Schema } from "mongoose";
 
+export interface IImage extends Document {
+    title: string;
+    transformationType : string
+    publicID:string
+    secureURL: string
+    width: number,
+    hight: number,
+    config:object,
+    transformationURL: string,
+    aspectRation: string,
+    color: string,
+    prompt: string
+    author: {
+        _id: string,
+        firstName: string,
+        lastName: string,
+    }
+    createdAt: Date,
+    updatedAt: Date,
+}
+
 
 const imageSchema = new Schema({
     title :{type: String , required: true},
