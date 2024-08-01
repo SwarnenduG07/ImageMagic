716017dcbf6a2340b4b645c193d85e275029fae7
Sunny <swarnendughosh05@gmail.com>
2024-08-02 01:41:28 +0530

    dynamic title and subtitle added

--- app/dashboard/transformations/add/[type]/page.tsx
+++ app/dashboard/transformations/add/[type]/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Header from '@/components/Header'
+import TransformationForm from '@/components/TransformationForm'
+import { transformationTypes } from '@/constants'
 
-const AddTransformationPage = () => {
+const AddTransformationPage = ({params: {type}}: SearchParamProps) => {
+ 
+  const transformation = transformationTypes[type]
+
   return (
-    <div>AddTransformationPage</div>
+    <>
+       <Header 
+         title={transformation.title}
+         subtitle={transformation.subTitle}
+       />
+         <TransformationForm />
+    </> 
   )
 }
 
