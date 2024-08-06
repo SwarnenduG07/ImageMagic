0c1bcf67c97694d5e691909434a6299702d0cf3f
Sunny <swarnendughosh05@gmail.com>
2024-08-07 00:02:18 +0530

    mobile nav bar fixed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,11 +6,18 @@ const Landing = () => {
     return (
         <div className="">
          <HomeNavBar/>
-          <div className="h-screen flex flex-col items-center justify-center text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-amber-500">
+         <span className="flex flex-col justify-center text-center pt-52 text-3xl text-emerald-500 font-bold">
+            AI Platform For 
+         </span>
+          <div className="pb-72  h-screen flex flex-col items-center justify-center text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-amber-500">
           <TypewriterComponent 
               options={{
                 strings: [
-                    "Under Development"
+                    "Under Development",
+                    "Backgound Removal",
+                    "Object Removal",
+                    "Color Changing",
+                    "Ratio Conversion"
                 ],
                 autoStart: true,
                 loop: true,
