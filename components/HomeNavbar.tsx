768dcde1398f805bc3867223ff456740dc528081
Sunny <swarnendughosh05@gmail.com>
2024-08-06 23:10:07 +0530

    mobilenav bar fixed

--- components/HomeNavbar.tsx
+++ components/HomeNavbar.tsx
@@ -10,11 +10,13 @@ export const HomeNavBar = () => {
     return (
          <nav className="fixed w-full  border-b border-purple-500 dark:border-gray-600">
           <div className="pt-5 pb-5 flex flex-wrap items-center justify-between">
-            <span className="text-3xl font-bold text-emerald-400 ml-16">
+            <span className="text-xl font-bold text-emerald-400 lg:ml-16 md:ml-16 lg:text-3xl md:text-2xl ml-5">
                IMAGE-MAGIC
             </span>
             <Link href={isSignedIn? "/dashboard" : "/sign-up" }>
-              <Button className="transition bg-amber-600 ease-in-out delay-100 hover:-translate-y-1 hover:scale-105 duration-200 hover:bg-teal-600 mr-10">
+              <Button className="transition bg-amber-600 ease-in-out delay-100 hover:-translate-y-1 hover:scale-105 duration-200 hover:bg-teal-600 mr-5 md:h-10 md:w-25 lg:h-10 lg:w-25 h-8 w-23 rounded-full
+              
+              ">
                  Get Started
                </Button>
              </Link>
