10fc7838475752e98d37b5da139c49c81f9e81a6
Sunny <swarnendughosh05@gmail.com>
2024-08-08 01:02:54 +0530

    Landing updated

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,10 +6,13 @@ const Landing = () => {
     return (
         <div className="">
          <HomeNavBar/>
-         <span className="flex flex-col justify-center text-center pt-52 text-3xl text-emerald-500 font-bold">
-            AI Platform For 
+         <span className="flex flex-col justify-center text-center pt-52 text-6xl text-emerald-500 font-bold">
+             The best AI Platform For
          </span>
-          <div className="pb-72  h-screen flex flex-col items-center justify-center text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-amber-500">
+         <span className="flex flex-col justify-center text-center pt-24 text-3xl text-slate-400 font-light">
+            No cradit Card Required
+         </span>
+          <div className="pb-44  h-screen flex flex-col items-center justify-center text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-amber-500">
           <TypewriterComponent 
               options={{
                 strings: [
