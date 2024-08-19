27d80b7700e3c3e2cfe44d82562385087a59820d
Sunny <swarnendughosh05@gmail.com>
2024-08-19 20:43:45 +0530

    landing Bg chenged

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,7 +1,7 @@
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='h-screen bg-white'>
+    <div className='h-screen bg-black'>
          <div>
             {children}
           </div>
--- components/LandingContent.tsx
+++ components/LandingContent.tsx
@@ -2,9 +2,23 @@
 import TypewriterComponent from "typewriter-effect"
 import ShinyButton from "./magicui/shiny-button"
 import Link from "next/link"
+import { SparklesCore } from "./ui/sparkles"
 
 const LandingContent = () => {
   return (
+    <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+
     <div>
           <span className="flex flex-col justify-center text-center pt-36 text-6xl text-emerald-500 font-bold">
              The best AI Platform For
@@ -26,7 +40,7 @@ const LandingContent = () => {
           </div>
           <Link href="/dashboard">
            <span className="flex flex-col items-center    mt-12">
-             <ShinyButton text="Get Started" className="rounded-full w-44 h-10 "/>
+             <ShinyButton text="Get Started" className="rounded-full w-56 h-10 "/>
             </span>
            </Link>
           <span className="flex flex-col justify-center text-center pt-10 pb-4 text-2xl text-slate-400 font-light">
@@ -34,6 +48,7 @@ const LandingContent = () => {
          </span>
 
     </div>
+  </div>  
   )
 }
 
